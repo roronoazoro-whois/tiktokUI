bf11427f6b029e66df392307d2d19a86a9d5a2a1
BANH VIET HUNG <roronoazoro.whois@gmail.com>
2024-08-03 11:35:20 +0700

    Cấu hình Router & Layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from './routes';
+import { DefaultLayout } from './components/Layout';
 
 function App() {
     return (
-        <div className="App">
-            <h1>Hello React</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {/* <Route path="/" element={<Home />} />
+                    <Route path="/following" element={<Following />} /> */}
+
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
